4cc9e9045c6036de7c600445f33b9f0242389028
Andret170170 <24122004@bk.ru>
2023-02-07 19:47:33 -0800

    1 more table

---
+++ RPM_Project_Library/RPM_PR_LIB/ListsHaveProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPM_PR_LIB;
+
+
+public partial class ListsHaveProduct
+{
+    public int LhpId { get; set; }
+
+    public int LhpPlId { get; set; }
+
+    public int LhpProId { get; set; }
+
+    public virtual ProductList LhpPl { get; set; } = null!;
+
+    public virtual Product LhpPro { get; set; } = null!;
+}
