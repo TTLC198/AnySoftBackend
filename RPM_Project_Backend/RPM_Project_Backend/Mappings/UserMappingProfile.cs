e29542bb3299b7a2b5dc3bfce5916bd4219bd5ea
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-03-22 19:09:03 +0300

    Adding JWT Authorization, Swagger, AutoMapper

---
+++ RPM_Project_Backend/RPM_Project_Backend/Mappings/UserMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using RPM_PR_LIB;
+
+namespace RPM_Project_Backend.Mappings;
+
+public class UserMappingProfile : Profile
+{
+    public UserMappingProfile()
+    {
+        CreateMap<UserDto, User>().ReverseMap();
+    }
+}
