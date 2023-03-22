29e4e5675dff49c8f11672cd4d999e525843effc
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-03-22 19:09:28 +0300

    Adding a new login user DTO entity

--- RPM_Project_Library/RPM_PR_LIB/User.cs
+++ RPM_Project_Library/RPM_PR_LIB/User.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
@@ -7,22 +9,30 @@ namespace RPM_PR_LIB;
 public class User : BaseModel
 {
     [Column("u_id")]
-    [ValidateNever]
     public override int Id { get; set; }
     [Column("u_login")]
-    [ValidateNever]
     public string Login { get; set; } = null!;
     [Column("u_password")]
-    [ValidateNever]
     public string Password { get; set; } = null!;
     [Column("u_email")]
-    [ValidateNever]
     public string Email { get; set; } = null!;
     [Column("u_role_id")]
-    [ValidateNever]
     public int RoleId { get; set; }
-
-    [ValidateNever]
+    
     public virtual Role Role { get; set; } = null!;
     public virtual ICollection<Address> Addresses { get; } = new List<Address>();
+}
+
+public class UserDto
+{
+    public string? Login { get; set; }
+
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "User Password is required!")]
+    public string? Password { get; set; }
+
+    [Range(typeof(bool), "true", "true", ErrorMessage = "At least one field must be given a value")]
+    public bool IsValidated =>
+        !string.IsNullOrWhiteSpace(Login) || !string.IsNullOrWhiteSpace(Email);
 }
