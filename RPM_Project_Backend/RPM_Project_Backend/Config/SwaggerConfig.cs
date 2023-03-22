b7c9249401a9a3f239a8f78b7f86751d424eafa9
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-03-22 19:08:01 +0300

    Splitting the startup class into separate configuration files.

---
+++ RPM_Project_Backend/RPM_Project_Backend/Config/SwaggerConfig.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace RPM_Project_Backend.Config;
+
+public static class SwaggerConfig
+{
+    public static void AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "RPM Project Rest API",
+                Description = "Developed by Vadim Kh. & Andrey K. 2023",
+                Contact = new OpenApiContact { Name = "Vadim Kh.", Email = "yorehacked@gmail.com" }
+            });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "Input the JWT like: Bearer {your token}",
+                Name = "Authorization",
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        });
+    }
+
+    public static void UseSwaggerConfiguration(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        });
+    }
+}
--- RPM_Project_Backend/RPM_Project_Backend/Startup.cs
+++ RPM_Project_Backend/RPM_Project_Backend/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using RPM_PR_LIB;
+using RPM_Project_Backend.Config;
 using RPM_Project_Backend.Services.Database;
 
 namespace RPM_Project_Backend;
@@ -31,14 +32,17 @@ public class Startup
             opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
         });
         services.AddControllers().AddNewtonsoftJson();
-#if DEBUG
+        // Use Swagger
         // Inject an implementation of ISwaggerProvider with defaulted settings applied.
         services.AddSwaggerGen();
-#endif
+        services.AddSwaggerConfiguration();
+        
+        services.AddJwtConfiguration(Configuration);
+        services.AddAutoMapperConfiguration();
     }
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-#if DEBUG
+        // Use Swagger
         app.UseSwaggerUI(c => {
             c.RoutePrefix = "swagger/ui";
             c.SwaggerEndpoint("/swagger/v1/swagger.json", "RPM_Project_APi v1");
@@ -46,19 +50,27 @@ public class Startup
         
         // Enable middleware to serve generated Swagger as a JSON endpoint
         app.UseSwagger();
-
         // Enable middleware to serve swagger-ui assets (HTML, JS, CSS etc.)
         app.UseSwaggerUI();
-#endif
+        app.UseSwaggerConfiguration();
+
+        
         
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
         }
+
         app.UseHttpsRedirection();
-        app.UseStaticFiles();
+        
+        app.UseAuthentication();
         app.UseRouting();
-        app.UseCors();
+        app.UseCors(c =>
+        {
+            c.AllowAnyHeader();
+            c.AllowAnyMethod();
+            c.AllowAnyOrigin();
+        });
         app.UseAuthorization();
         app.UseEndpoints(endpoints =>
         {
