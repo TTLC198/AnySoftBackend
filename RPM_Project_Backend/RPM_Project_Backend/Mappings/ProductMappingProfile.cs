86bec8c6c57c9b0f8dc3d2fe310237475fb748f8
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-03-25 23:05:05 +0300

    Added an override profile for a product and its DTO.

---
+++ RPM_Project_Backend/RPM_Project_Backend/Mappings/ProductMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using RPM_PR_LIB;
+
+namespace RPM_Project_Backend.Mappings;
+
+public class ProductMappingProfile : Profile
+{
+    public ProductMappingProfile()
+    {
+        CreateMap<ProductDto, Product>().ReverseMap();
+    }
+}
