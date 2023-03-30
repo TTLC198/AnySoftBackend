53d7aa79eb0d258b65fe5a9b962993e8f26c7b68
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-03-30 13:53:20 +0300

    Fixed data validation when adding a new image.

--- RPM_Project_Library/RPM_PR_LIB/Image.cs
+++ RPM_Project_Library/RPM_PR_LIB/Image.cs
@@ -1,7 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Http;
-using RPM_Project_Backend.Enums;
+using RPM_PR_LIB.Enums;
 
 namespace RPM_PR_LIB;
 
@@ -19,7 +20,7 @@ public class Image
     public string ContentType { get; set; }
     
     [Column("img_description")]
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     [Column("img_path"), Required]
     public string ImagePath { get; set; }
@@ -30,8 +31,13 @@ public class Image
 
 public class ImageDto
 {
+    [Required]
     public int ResourceId { get; set; }
+    [Required]
+    [Range(0,1, ErrorMessage = "Wrong type")]
     public ImageType Type { get; set; }
-    public string Description { get; set; }
+    
+    public string? Description { get; set; }
+    [Required]
     public IFormFile Image { get; set; }
 }
