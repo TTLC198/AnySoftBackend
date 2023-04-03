733c3fd81a24e2487317a40a46c401b891100198
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-04-03 20:58:29 +0300

    A unit test project has been added.

---
+++ RPM_Project_Backend/RPM_Project_Backend.Tests/TestValues.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RPM_Project_Backend.Tests;
+
+public static class TestValues
+{
+    public static IReadOnlyList<Role> Roles = new List<Role>()
+    {
+        new ()
+        {
+            Id = 1, Name = "admin"
+        },
+        new ()
+        {
+            Id = 2, Name = "user"
+        },
+        new ()
+        {
+            Id = 3, Name = "seller"
+        },
+    };
+    
+    public static IReadOnlyList<User> Users = new List<User>()
+    {
+        new ()
+        {
+            Id = 1,
+            Email = "ex1@email.com",
+            Login = "ex1",
+            Password = "expass1",
+            RoleId = 1,
+            Role = Roles[0]
+        },
+        new ()
+        {
+            Id = 2,
+            Email = "ex2@email.com",
+            Login = "ex2",
+            Password = "expass2",
+            RoleId = 2,
+            Role = Roles[1]
+        },
+        new ()
+        {
+            Id = 3,
+            Email = "ex3@email.com",
+            Login = "ex3",
+            Password = "expass3",
+            RoleId = 3,
+            Role = Roles[2]
+        },
+    };
+
+    public static User SingleUser = new()
+    {
+        Id = 4,
+        Email = "user@email.com",
+        Login = "user",
+        Password = "userpass",
+        RoleId = 2,
+        Role = Roles[1]
+    };
+}
