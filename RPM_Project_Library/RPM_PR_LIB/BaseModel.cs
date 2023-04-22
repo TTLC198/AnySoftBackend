79cd4de914d7458e4c889a700cc010b5ecd6a99d
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-04-22 17:06:09 +0300

    Removed the inheritance of classes from the general model (deprecated from the time of using the repository pattern)

--- RPM_Project_Library/RPM_PR_LIB/BaseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace RPM_PR_LIB;
-
-public abstract class BaseModel
-{
-    public abstract int Id { get; set; }
-}
