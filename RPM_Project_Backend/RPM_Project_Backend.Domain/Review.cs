757943274c96f4b4c9e024316bf4b8db05219ade
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-04-29 22:30:09 +0300

    Added ReviewResponseDto, added Review creation date

--- RPM_Project_Backend/RPM_Project_Backend.Domain/Review.cs
+++ RPM_Project_Backend/RPM_Project_Backend.Domain/Review.cs
@@ -13,7 +13,7 @@ public class Review
     [Column("rew_u_id"), Required]
     public int UserId { get; set; }
 
-    [Column("rew_text"), Required, StringLength(50)]
+    [Column("rew_text"), Required, StringLength(256)]
     public string? Text { get; set; }
 
     [Column("rew_grade"), Required]
@@ -21,6 +21,9 @@ public class Review
 
     [Column("rew_pro_id"), Required]
     public int ProductId { get; set; }
+    
+    [Column("img_ts"), Required]
+    public DateTime Ts { get; set; }
 
     [ValidateNever]
     [ForeignKey("ProductId")]
@@ -28,5 +31,28 @@ public class Review
 
     [ValidateNever]
     [ForeignKey("UserId")]
+    [NotMapped]
     public virtual User? User { get; }
+}
+/// <summary>
+/// Review object that is returned when requested
+/// </summary>
+public class ReviewResponseDto
+{
+    /// <summary>
+    /// Review Text
+    /// </summary>
+    public string? Text { get; set; }
+    /// <summary>
+    /// Review Grade
+    /// </summary>
+    public double Grade { get; set; }
+    /// <summary>
+    /// Review creation date
+    /// </summary>
+    public DateTime Ts { get; set; }
+    /// <summary>
+    /// Review User entity
+    /// </summary>
+    public UserResponseDto? User { get; set; }
 }
