53c7692027cce0bde749e08d8b5af395e36ddbd8
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-01 14:19:43 +0300

    Added property controller: main logic, mapping profile, propertyDto

--- RPM_Project_Backend/RPM_Project_Backend.Domain/Property.cs
+++ RPM_Project_Backend/RPM_Project_Backend.Domain/Property.cs
@@ -21,3 +21,17 @@ public class Property
     [JsonIgnore]
     public virtual IEnumerable<ProductsHaveProperties>? ProductsHaveProperties { get; }
 }
+/// <summary>
+/// Property Data Transfer Object
+/// </summary>
+public class PropertyDto
+{
+    /// <summary>
+    /// Property Name
+    /// </summary>
+    public string? Name { get; set; }
+    /// <summary>
+    /// Property Product Id
+    /// </summary>
+    public int? ProductId { get; set; }
+}
---
+++ RPM_Project_Backend/RPM_Project_Backend/Mappings/PropertyMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using RPM_Project_Backend.Domain;
+
+namespace RPM_Project_Backend.Mappings;
+
+public class PropertyMappingProfile : Profile
+{
+    public PropertyMappingProfile()
+    {
+        CreateMap<PropertyDto, Property>().ReverseMap();
+    }
+}
