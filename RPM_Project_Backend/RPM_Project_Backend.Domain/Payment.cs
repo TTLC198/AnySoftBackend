241853ef706fca2f9515b387d837eb54e208ec89
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-01 17:56:27 +0300

    Added PaymentEditDto

--- RPM_Project_Backend/RPM_Project_Backend.Domain/Payment.cs
+++ RPM_Project_Backend/RPM_Project_Backend.Domain/Payment.cs
@@ -44,3 +44,25 @@ public class PaymentDto
     /// </summary>
     public string? Cvc { get; set; }
 }
+/// <summary>
+/// Payment Edit Object
+/// </summary>
+public class PaymentEditDto
+{
+    /// <summary>
+    /// Identifier
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Bank Card Number
+    /// </summary>
+    public string? Number { get; set; }
+    /// <summary>
+    /// Bank Card Expiration Date
+    /// </summary>
+    public DateTime ExpirationDate { get; set; }
+    /// <summary>
+    /// Bank Card Security Code
+    /// </summary>
+    public string? Cvc { get; set; }
+}
