51a631443f5db10b8d32c0a2eb0a407c3b734b62
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-26 20:02:40 +0300

    Added misc classes in separated directory

---
+++ RPM_Project_Backend/AnySoftBackend.Library/Misc/SortOrderObject.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace AnySoftBackend.Library.Misc;
+
+public class SortOrderObject
+{
+    [ValidateNever]
+    public string? Type { get; set; }
+    [ValidateNever]
+    public int? Direction { get; set; }
+}
