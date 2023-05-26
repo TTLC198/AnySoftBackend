43b45a4329e05518482fa36730799098744173d8
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-26 20:02:58 +0300

    Reinvented MinMaxObject ;)

---
+++ RPM_Project_Backend/AnySoftBackend.Library/Misc/MinMaxObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Newtonsoft.Json;
+using RPM_Project_Backend.Helpers;
+
+namespace AnySoftBackend.Library.Misc;
+
+public class MinMaxObject<T> where T: struct
+{
+    [ValidateNever]
+    public T? Min { get; set; }
+    [ValidateNever]
+    public T? Max { get; set; }
+}
+
+public class MinMaxDateTimeObject : MinMaxObject<DateTime>
+{
+    [ValidateNever]
+    [JsonConverter(typeof(JsonDateTimeConverter))]
+    public new DateTime? Min { get; set; }
+    [ValidateNever]
+    [JsonConverter(typeof(JsonDateTimeConverter))]
+    public new DateTime? Max { get; set; }
+}
