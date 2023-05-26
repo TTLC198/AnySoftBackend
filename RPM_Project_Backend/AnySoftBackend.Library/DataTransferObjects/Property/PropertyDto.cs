4296ee3020e9e538d7ac3bd5c34a6ee0b5d2d0aa
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-26 20:00:26 +0300

    Added DTOs to new project's library

---
+++ RPM_Project_Backend/AnySoftBackend.Library/DataTransferObjects/Property/PropertyDto.cs
@@ -0,0 +1,16 @@
+﻿namespace AnySoftBackend.Library.DataTransferObjects.Property;
+
+/// <summary>
+/// Property Data Transfer Object
+/// </summary>
+public class PropertyDto
+{
+    /// <summary>
+    /// Property Name
+    /// </summary>
+    public string? Name { get; set; }
+    /// <summary>
+    /// Property Product Id
+    /// </summary>
+    public int? ProductId { get; set; }
+}
---
+++ RPM_Project_Backend/AnySoftBackend.Library/DataTransferObjects/User/UserEditDto.cs
@@ -0,0 +1,24 @@
+﻿namespace AnySoftBackend.Library.DataTransferObjects.User;
+
+/// <summary>
+/// User Data Transfer Object
+/// </summary>
+public class UserEditDto
+{
+    /// <summary>
+    /// Identifier
+    /// </summary>
+    public int? Id { get; set; }
+    /// <summary>
+    /// Login
+    /// </summary>
+    public string? Login { get; set; }
+    /// <summary>
+    /// Email
+    /// </summary>
+    public string? Email { get; set; }
+    /// <summary>
+    /// Password
+    /// </summary>
+    public string? Password { get; set; }
+}
